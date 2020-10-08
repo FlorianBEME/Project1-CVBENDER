d4e9285fcc36fe7917bfbea9b1d9a3e9b2ad7042
FlorianBEME <florianbme@gmail.com>
2020-10-08 15:58:27 +0200

    fix form.js

--- form.js
+++ form.js
@@ -1,4 +1,7 @@
 document.getElementById("formu").addEventListener("submit", function (event) {
+  let firstNameValue = document.getElementById('firstname').value;
     event.preventDefault();
+    document.querySelector('.firstName').innerHTML= "Merci "+ firstNameValue.charAt(0).toUpperCase() + firstNameValue.substr(1).toLowerCase() + ", votre message à été envoyé avec succès."
+  console.log(firstNameValue);
     document.getElementById("phrasingForm").style.display = "flex";
   });
