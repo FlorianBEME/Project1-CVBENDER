3d84691b33f08828b6fab3235633b1ff84663579
FlorianBEME <florianbme@gmail.com>
2020-10-09 09:42:55 +0200

    fix bug

--- form.js
+++ form.js
@@ -1,7 +1,7 @@
 document.getElementById("formu").addEventListener("submit", function (event) {
   let firstNameValue = document.getElementById('firstname').value;
     event.preventDefault();
-    document.querySelector('.firstName').innerHTML= "Merci "+ firstNameValue.charAt(0).toUpperCase() + firstNameValue.substr(1).toLowerCase() + ", votre message à été envoyé avec succès."
+    document.querySelector('.firstName').innerHTML= "Merci "+ firstNameValue.charAt(0).toUpperCase() + firstNameValue.substr(1).toLowerCase() + ", votre message a été envoyé avec succès."
   console.log(firstNameValue);
     document.getElementById("phrasingForm").style.display = "flex";
   });
